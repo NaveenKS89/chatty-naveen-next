5c4664a4febc4aa8e252de99ba257e598855968a
NaveenKS89 <naveenkalburgis@gmail.com>
2026-04-16 19:19:08 +0530

    remove matcher

--- middleware.js
+++ middleware.js
@@ -3,5 +3,6 @@ import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0/edge";
 export default withMiddlewareAuthRequired();
 
 export const config = {
-  matcher: ["/api/chat/:path*", "/chat/:path*"],
+  //matcher: ["/api/chat/:path*", "/chat/:path*"],
+  matcher: ["/chat/:path*"],
 };
